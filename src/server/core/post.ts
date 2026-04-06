a7b23f425ca4f7f7c74744c3e399be7a4acbce91
Martin <m@rtin.ms>
2026-04-06 14:46:34 +0200

    accept change
    Signed-off-by: Martin <m@rtin.ms>

--- src/server/core/post.ts
+++ src/server/core/post.ts
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 import { reddit } from '@devvit/web/server';
-=======
-import { reddit, context } from '@devvit/web/server';
->>>>>>> 67678da7dda8f3ef01a0dd7906262bba9f8c3d8c
 
 export const createPost = async () => {
   return await reddit.submitCustomPost({
     title: 'feed-automator',
-<<<<<<< HEAD
-=======
-    subredditName: context.subredditName,
->>>>>>> 67678da7dda8f3ef01a0dd7906262bba9f8c3d8c
   });
 };
